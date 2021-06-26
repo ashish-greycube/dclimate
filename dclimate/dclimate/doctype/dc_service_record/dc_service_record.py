1531986557b9a9f94cdb7660cd8c89f00e6d7124
ashish-greycube <mr.ashish.shah@gmail.com>
2021-06-26 10:07:16 +0530

    feat: single line item price for parts item

--- dclimate/dclimate/doctype/dc_service_record/dc_service_record.py
+++ dclimate/dclimate/doctype/dc_service_record/dc_service_record.py
@@ -10,12 +10,14 @@ from erpnext import get_default_company
 class DCServiceRecord(Document):
 	def on_submit(self):
 		purchase_invoice=make_purchase_invoice(self.name)
-		frappe.msgprint(msg=_("Purchase Invoice {0} is created based on DC Service Record {1}"
-		.format(frappe.bold(get_link_to_form("Purchase Invoice",purchase_invoice)),frappe.bold(self.name))),
-		title="Purchase Invoice is created.",
-		indicator="green")
+		if purchase_invoice!=0:
+			frappe.msgprint(msg=_("Purchase Invoice {0} is created based on DC Service Record {1}"
+			.format(frappe.bold(get_link_to_form("Purchase Invoice",purchase_invoice)),frappe.bold(self.name))),
+			title="Purchase Invoice is created.",
+			indicator="green")
 
 	def validate(self):
+		# validate job price is present in supplier price list and calculate total hours and cost
 		total_srt_hours=0
 		total_srt_cost=0
 		supplier_price_list=self.supplier_price_list
@@ -30,8 +32,6 @@ class DCServiceRecord(Document):
 			'valid_from': ['<=', nowdate()]
 				},
 				fields=['price_list_rate'])
-				print('-'*100)
-				print(job_cost_by_supplier)
 				if job_cost_by_supplier and job_cost_by_supplier[0]:
 					total_srt_cost=total_srt_cost+job_item.hours*job_cost_by_supplier[0].get('price_list_rate')
 				else:
@@ -41,38 +41,78 @@ class DCServiceRecord(Document):
 					total_srt_hours=total_srt_hours+job_item.hours
 			self.total_srt_hours=total_srt_hours
 			self.total_srt_cost=total_srt_cost
+		
+		# validate repair part price is present in buy back price list
+		default_parts_under_warranty_replacement_service_item=frappe.db.get_single_value('DClimate Settings', 'default_parts_under_warranty_replacement_service_item')
+		parts_buy_back_margin_price_list=frappe.db.get_single_value('DClimate Settings', 'parts_buy_back_margin_price_list')
+		warranty_replacement_service_item_rate=0
+		for item in self.parts_detail:
+			replacement_service_item_rate=frappe.db.get_all('Item Price', filters={
+																		'price_list': ['=', parts_buy_back_margin_price_list],
+																		'item_code': ['=', item.item]
+																		}, fields='price_list_rate')
+			if not replacement_service_item_rate:
+				frappe.throw(msg=_("Item price for part no {0} not found in parts buy back price list {1}. Please contact DClimate"
+						.format(frappe.bold(item.item),frappe.bold(parts_buy_back_margin_price_list))),title='Repair part price not found error.')			
 
 
 @frappe.whitelist()
 def make_purchase_invoice(source_name, target_doc=None):
 	from frappe.model.mapper import get_mapped_doc
-	from erpnext.accounts.party import get_payment_terms_template
+	# from erpnext.accounts.party import get_payment_terms_template
 
 	doc = frappe.get_doc('DC Service Record', source_name)
+	if doc.parts_warranty_status!='Under Warranty' and doc.labor_warranty_status!='Under Warranty':
+		frappe.msgprint(msg=_("Parts warranty status is {0} and labour warranty status is {1}. Hence there are <b>no items</b> to create a Purchase Invoice."
+				.format(frappe.bold(doc.parts_warranty_status),frappe.bold(doc.labor_warranty_status)))
+				,title='Purchase Invoice is not created.',indicator="yellow")	
+		return 0	
+
+	# prepare single item and price for repair parts
+	default_parts_under_warranty_replacement_service_item=frappe.db.get_single_value('DClimate Settings', 'default_parts_under_warranty_replacement_service_item')
+	parts_buy_back_margin_price_list=frappe.db.get_single_value('DClimate Settings', 'parts_buy_back_margin_price_list')
+	warranty_replacement_service_item_rate=0
+	if doc.parts_warranty_status=='Under Warranty':
+		for item in doc.parts_detail:
+			replacement_service_item_rate=frappe.db.get_all('Item Price', filters={
+																		'price_list': ['=', parts_buy_back_margin_price_list],
+																		'item_code': ['=', item.item]
+																		}, fields='price_list_rate')
+			if replacement_service_item_rate:
+				replacement_service_item_rate=replacement_service_item_rate[0].price_list_rate
+			else:
+				frappe.throw(msg=_("Item price for part no {0} not found in parts buy back price list {1}. Please contact DClimate"
+						.format(frappe.bold(item.item),frappe.bold(parts_buy_back_margin_price_list))),title='Repair part price not found error.')
+			warranty_replacement_service_item_rate+=replacement_service_item_rate*item.qty															
+
 	def set_missing_values(source, target):
 		target.set_posting_time=1
 		target.posting_date=getdate(source.completion_date_time)
 		target.posting_time=get_time(source.completion_date_time)
+
+		if source.parts_warranty_status=='Under Warranty':
+			target.append('items',{
+			"item_code":default_parts_under_warranty_replacement_service_item,
+			"qty" :1,
+			"rate":warranty_replacement_service_item_rate				
+			})
+
+		if source.labor_warranty_status=='Under Warranty':
+			for item in source.job_codes:
+				target.append('items',{
+				"item_code":item.job_code,
+				"qty" :item.hours,
+				})
+
 		if len(target.get("items")) == 0:
 			frappe.throw(_("All items have already been Invoiced/Returned"))
 
 		doc = frappe.get_doc(target)
-		doc.ignore_pricing_rule = 1
 		doc.bill_no = source.name
 		# doc.payment_terms_template = get_payment_terms_template(source.service_by_supplier, "Supplier",get_default_company())
 		# doc.run_method("onload")
 		doc.run_method("set_missing_values")
 		doc.run_method("calculate_taxes_and_totals")
-
-	# def update_parts_item(source_doc, target_doc, source_parent):
-	# 	if source_parent.parts_warranty_status=='Under Warranty':
-	# 		target_doc.item_code =source_doc.item
-	# 		target_doc.qty =source_doc.qty
-
-	# def update_job_item(source_doc, target_doc, source_parent):
-	# 	if source_parent.labor_warranty_status=='Under Warranty':
-	# 		target_doc.item_code =source_doc.job_code
-	# 		target_doc.qty =source_doc.hours
 
 	doclist = get_mapped_doc("DC Service Record", source_name,	{
 		"DC Service Record": {
@@ -87,22 +127,6 @@ def make_purchase_invoice(source_name, target_doc=None):
 				"docstatus": ["=", 1],
 			},
 		},
-		"DC Service Record Parts Detail": {
-			"doctype": "Purchase Invoice Item",
-			"field_map": {
-				"item": "item_code",
-				"qty": "qty",
-			},
-			# "postprocess": update_parts_item,
-		},
-		"DC Service Record Job Codes Detail": {
-			"doctype": "Purchase Invoice Item",
-			"field_map": {
-				"job_code": "item_code",
-				"hours": "qty",
-			},
-			# "postprocess": update_job_item,
-		},		
 		"Purchase Taxes and Charges": {
 			"doctype": "Purchase Taxes and Charges",
 			"add_if_empty": True
