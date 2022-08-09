b5d8bc0d6f1be7d3df48609c09de35911f8f775f
ashish-greycube <mr.ashish.shah@gmail.com>
2022-08-09 10:11:07 +0530

    fix: unique serialno check, remove srt_cost and supplier from campaign

--- dclimate/dclimate/doctype/dc_campaign/dc_campaign.py
+++ dclimate/dclimate/doctype/dc_campaign/dc_campaign.py
@@ -10,6 +10,7 @@ from frappe.contacts.doctype.address.address import get_address_display
 class DCCampaign(Document):
 	@frappe.whitelist()
 	def sync_dc_campaign_completion_form(self):
+		self.validate_unqiue_serial_no_in_campaign()
 		for serial_detail in self.dc_campaign_serial_no:
 			if serial_detail.dc_campaign_completion_form:
 				status=frappe.db.get_value('DC Campaign Completion Form', serial_detail.dc_campaign_completion_form, 'status')
@@ -50,11 +51,6 @@ class DCCampaign(Document):
 		completion_form.labor_warranty_expiry_date=frappe.db.get_value("Serial No", serial_no, 'labor_warranty_expiry_date_cf')
 		if completion_form.labor_warranty_expiry_date:
 			completion_form.labor_warranty_status='Under Warranty' if (getdate(completion_form.labor_warranty_expiry_date) >=getdate()) else 'Warranty Expired'
-		completion_form.service_by_supplier=self.supplier
-		supplier_address=frappe.db.get_list('Dynamic Link',  fields=['parent'],filters={'link_doctype': 'Supplier','parentfield' : 'links','parenttype' :'Address','link_name':completion_form.service_by_supplier})
-		if len(supplier_address)>0:
-			completion_form.supplier_address=supplier_address[0].parent
-			completion_form.supplier_address_display=get_address_display(completion_form.supplier_address)
 		completion_form.status='Open'
 		completion_form.issue_description=self.description
 		completion_form.default_parts_issue_warehouse=self.default_parts_issue_warehouse
@@ -71,30 +67,38 @@ class DCCampaign(Document):
 			cf_jobs_child.job_code=job.job_code
 			cf_jobs_child.hours=job.hours
 		completion_form.total_srt_hours=self.total_srt_hours
-		completion_form.total_srt_cost=self.total_srt_cost
 		completion_form.run_method('set_missing_values')
 		completion_form.save(ignore_permissions=True)
 		return completion_form.name,completion_form.status
 
 	def validate(self):
+		self.validate_unqiue_serial_no_in_campaign()
 		self.calculate_total_srt_values()
+		self.validate_complete_status()
+
+	def validate_complete_status(self):
+		if self.status=='Complete':
+			for row in self.dc_campaign_serial_no:
+				if row.status!='Finished':
+					frappe.throw(_("Row {0} : Campaign Completion Form {1}, status is {2}. <br> Please finish  all campaign completion form in order to mark campaign {3} as Complete")
+					.format(row.idx,row.dc_campaign_completion_form, frappe.bold(row.status), self.name))
+
+	def validate_unqiue_serial_no_in_campaign(self):
+		serial_no_list=[]
+		for row in self.dc_campaign_serial_no:
+			if row.serial_no not in serial_no_list:
+				serial_no_list.append(row.serial_no)
+			else:
+				frappe.throw(_("Row {0} : Serial no {1} is duplicate".format(row.idx,frappe.bold(row.serial_no))))
 
 	def calculate_total_srt_values(self):
 		# validate job price is present in supplier price list and calculate total hours and cost
 		total_srt_hours=0
-		total_srt_cost=0
-		# supplier_price_list=self.supplier_price_list
-		per_hour_rate_cf = frappe.db.get_value('Supplier', self.supplier, 'per_hour_rate_cf')
-		if not per_hour_rate_cf:
-			frappe.throw(msg=_('Per hour job rate is not defined for supplier {0}. Please contact DClimate'.format(frappe.bold(get_link_to_form('Supplier',self.supplier)))),title="Missing per hour job rate for supplier.")
 		if self.job_codes:
 			for job_item in self.job_codes:
-				if job_item.job_code:
-					total_srt_cost=total_srt_cost+job_item.hours*per_hour_rate_cf
 				if job_item.hours:
 					total_srt_hours=total_srt_hours+job_item.hours
 			self.total_srt_hours=total_srt_hours
-			self.total_srt_cost=total_srt_cost
 		
 		# validate repair part price is present in buy back price list
 		default_parts_under_warranty_replacement_service_item=frappe.db.get_single_value('DClimate Settings', 'default_parts_under_warranty_replacement_service_item')
