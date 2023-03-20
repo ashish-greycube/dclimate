2fb18bb7c91dd58f7aa990abfb869b0f350d6916
ashish-greycube <mr.ashish.shah@gmail.com>
2023-03-20 16:59:30 +0530

    fix: width, sorting, formatting etc

--- dclimate/dclimate/report/dclimate_item_shortage_report/dclimate_item_shortage_report.js
+++ dclimate/dclimate/report/dclimate_item_shortage_report/dclimate_item_shortage_report.js
@@ -3,39 +3,42 @@
 /* eslint-disable */
 
 frappe.query_reports["DClimate Item Shortage Report"] = {
-	"filters": [
-
-		
-		{
-            "fieldname":"item_group",
-            "label": 'Item Group',
-            "fieldtype": "Link",
-            "options": "Item Group",
-			   
-		},
-		{
-            "fieldname":"item_code",
-            "label": 'Item Code',
-            "fieldtype": "Link",
-            "options": "Item",
-			   
-		},
-		{
-            "fieldname":"warehouse",
-            "label": 'Warehouse',
-            "fieldtype": "Link",
-		"options": "Warehouse",
-			
-			   
-		},
-		{
-            "fieldname":"type",
-            "label": 'Type',
-            "fieldtype": "Select",
-            "options": ["All","Only Shortage"],
-			   
-		},
-            
-
-	]
+      "filters": [
+            {
+                  "fieldname": "warehouse",
+                  "label": 'Warehouse',
+                  "fieldtype": "Link",
+                  "options": "Warehouse",
+            },            
+            {
+                  "fieldname": "item_group",
+                  "label": 'Item Group',
+                  "fieldtype": "Link",
+                  "options": "Item Group",
+            },
+            {
+                  "fieldname": "item_code",
+                  "label": 'Item Code',
+                  "fieldtype": "Link",
+                  "options": "Item",
+            },
+            {
+                  "fieldname": "type",
+                  "label": 'Type',
+                  "fieldtype": "Select",
+                  "options": ["All", "Only Shortage"],
+                  "default": "All",
+            }
+      ],
+      formatter: function (value, row, column, data, default_formatter) {
+            value = default_formatter(value, row, column, data);
+            let type = frappe.query_report.get_filter_value('type')
+            if (type == 'All') {
+                  if (data.buffer_stock <= 0) {
+                        value = '<b style="color:red">' + value + '</b>';
+                  }
+            }
+            // for other normal 
+            return value;
+      },
 };
--- dclimate/dclimate/report/dclimate_item_shortage_report/dclimate_item_shortage_report.py
+++ dclimate/dclimate/report/dclimate_item_shortage_report/dclimate_item_shortage_report.py
@@ -3,57 +3,62 @@
 
 import frappe
 from frappe import _
-
-
+from frappe.utils import flt
 
 def execute(filters=None):
-# def execute(filters):
 	columns = [
-			
 			{
 				"label": "Item Code",
 				"fieldname":"item_code",
-				"fieldtype": "link",
-				"options":"Item"
+				"fieldtype": "Link",
+				"options":"Item",
+				"width":300
 			},
 			{
 				"label": 'Item Name',
 				"fieldname":"item_name",
-				"fieldtype": "Data"
+				"fieldtype": "Data",
+				"width":0,
+				"hidden":1
 
 			},
 			{
 				"label": 'Item Group',
 				"fieldname":"item_group",
-				"fieldtype": "link",	
-				"options":"Item Group"				
-			},
-			
-			{
-				"label": 'Safety Stock',
-				"fieldname":"safety_stock",
-				"fieldtype": "Data",	
+				"fieldtype": "Link",	
+				"options":"Item Group",
+				"width":180			
 			},
 			{
 				"label": 'Warehouse',
 				"fieldname":"warehouse",
-				"fieldtype": "link",
-				"options":"Warehouse"
-			},
+				"fieldtype": "Link",
+				"options":"Warehouse",
+				"width":200
+			},	
 			{
 				"label": 'Actual Qty',
 				"fieldname":"actual_qty",
-				"fieldtype": "Data",	
+				"fieldtype": "Float",	
+				"width":120
+			},					
+			{
+				"label": 'Safety Stock',
+				"fieldname":"safety_stock",
+				"fieldtype": "Float",
+				"width":120	
 			},
 			{
 				"label": 'Buffer Stock',
 				"fieldname":"buffer_stock",
-				"fieldtype": "Data",	
-			},
+				"fieldtype": "Float",	
+				"width":120
+			},			
 			{
-				"label": 'Shortage Qty Reference',
+				"label": 'Shortage Qty Ref',
 				"fieldname":"shortage_qty_reference",
-				"fieldtype": "Data",	
+				"fieldtype": "Float",
+				"width":140	
 			},
 		]
 	
@@ -61,59 +66,30 @@ def execute(filters=None):
 	query_filters = {}
 	query_filters_warehouse ={}
 
-	fields = [
-		"item_code",
-		"item_name",
-		"item_group",
-		"safety_stock",	
-	]
-	fields_warehouse = [
-		"item_code",
-		"warehouse",
-		"actual_qty"	
-	]
-	print('-'*10)
-	print('filters',filters)
-	
-	print('Type filter',filters.get("type"))
-
+	fields_item = ["item_code","item_name","item_group",	"safety_stock",	]
+	fields_warehouse = ["item_code","warehouse","actual_qty"]
 
 	for field in ["item_code", "item_group"]:
 		if filters.get(field):
 			query_filters[field] = ("in", filters.get(field))
 
-	print('query_filters',query_filters)
-
 	for field in ["item_code","warehouse"]:
 		if filters.get(field):
 			query_filters_warehouse[field] = ("in", filters.get(field))
-	print('query_filters_warehouse',query_filters_warehouse)
 
-
-	items = frappe.get_all("Item", fields=fields, filters=query_filters)
+	items = frappe.get_all("Item", fields=fields_item, filters=query_filters)
 	bins = frappe.get_all("Bin", fields=fields_warehouse, filters=query_filters_warehouse)
-	# print('items',items)
-	# print('bins',bins)
-	print('-'*10)
-
 
 	for bin in bins:
 		for item in items:
 			if bin.item_code == item.item_code:	
-
-				buffer_stock = bin.actual_qty - item.safety_stock
-
-				baq = float(bin.actual_qty)
-				iss = item.safety_stock
-
-
+				buffer_stock = flt(bin.actual_qty) - flt(item.safety_stock)
+				baq = flt(bin.actual_qty)
+				iss = flt(item.safety_stock)
 				if(iss > 0):
 					shortage_qty_reference = baq / iss
 				else:
 					shortage_qty_reference = 0
-
-
-				print('***',filters.get("type") == 'Only Shortage',buffer_stock)
 
 				if(filters.get("type") == 'Only Shortage'):
 					if(buffer_stock <= 0):
@@ -121,24 +97,23 @@ def execute(filters=None):
 						item.item_code,
 						item.item_name,
 						item.item_group,
-						item.safety_stock,
 						bin.warehouse,
-						bin.actual_qty,
-						buffer_stock,
-						shortage_qty_reference	
+						flt(bin.actual_qty,1),
+						flt(item.safety_stock,1),
+						flt(buffer_stock,1),
+						flt(shortage_qty_reference,1)	
 				])
 				else:
 					data.append([
-					item.item_code,
-					item.item_name,
-					item.item_group,
-					item.safety_stock,
-					bin.warehouse,
-					bin.actual_qty,
-					buffer_stock,
-					shortage_qty_reference	
+						item.item_code,
+						item.item_name,
+						item.item_group,
+						bin.warehouse,
+						flt(bin.actual_qty,1),
+						flt(item.safety_stock,1),
+						flt(buffer_stock,1),
+						flt(shortage_qty_reference,1)		
 				])
-
-				# 	for item in items:
-				# 		if(buffer_stock == 0)
+	# sort by shortage_qty_reference desc and item_code
+	data = sorted(data, key = lambda x: (x[7], x[0]))			
 	return columns, data
