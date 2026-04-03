f73634bc74414de315cefbf0e7f65587bb9287bb
jigar-greycube <jigar@greycube.in>
2026-04-03 16:09:26 +0530

    fix:fixed duplicate entry error in serial no warranty details report

--- dclimate/dclimate/report/serial_no_warranty_details/serial_no_warranty_details.py
+++ dclimate/dclimate/report/serial_no_warranty_details/serial_no_warranty_details.py
@@ -36,6 +36,18 @@ def get_columns():
 			"width" : 140
 		},
 		{
+			"fieldname" : "labor_warranty_expiry_date",
+			"label" : "Labor Warranty Expiry Date",
+			"fieldtype" : "Date",
+			"width" : 140
+		},
+		{
+			"fieldname" : "amc_expiry_date",
+			"label" : "AMC Expiry Date",
+			"fieldtype" : "Date",
+			"width" : 140
+		},
+		{
 			"fieldname" : "installation_note",
 			"label" : "Installation Note",
 			"fieldtype" : "Link",
@@ -91,7 +103,7 @@ def get_data(filters):
 	conditions = ""
 	
 	if filters.get("serial_no"):
-		conditions = " where sn.name = '{0}'".format(filters["serial_no"])
+		conditions = " AND sn.name = '{0}'".format(filters["serial_no"])
 		
 
 	data = frappe.db.sql(
@@ -100,40 +112,32 @@ def get_data(filters):
 			sn.name AS "serial_number",
 			sn.item_code AS "item_code",
 			sn.warranty_expiry_date AS "warranty_expiry_date",    
-			inst_note.name AS "installation_note",
-			inst_note.customer AS "customer_name",    
-			dn_bundle.voucher_no AS "delivery_note",
-			dn_bundle.posting_date AS "latest_delivery_note_date",    
-			si_bundle.voucher_no AS "sales_invoice",
-			si_bundle.posting_date AS "latest_sales_invoice_date",    
-			se_bundle.voucher_no AS "stock_entry",
-			se_bundle.posting_date AS "latest_stock_entry_date"
+			sn.labor_warranty_expiry_date_cf as "labor_warranty_expiry_date",
+			sn.amc_expiry_date as "amc_expiry_date",
+			sn.installation__note_cf AS "installation_note",
+			sn.customer AS "customer_name",    
+			si.name as sales_invoice,
+			si.posting_date as latest_sales_invoice_date,
+			dn.name as delivery_note,
+			dn.posting_date as latest_delivery_note_date,
+			se_bundle.voucher_no as stock_entry,
+			se_bundle.posting_date as latest_stock_entry_date
 		FROM
 			`tabSerial No` sn
-		JOIN
-			`tabInstallation Note Item` in_item ON in_item.serial_no LIKE CONCAT('%', sn.name, '%')
-		JOIN
-			`tabInstallation Note` inst_note ON inst_note.name = in_item.parent AND inst_note.docstatus = 1
-		LEFT JOIN (
-			SELECT 
-				sbe.serial_no,  sbb.voucher_no  AS voucher_no,
-				sbb.posting_date AS posting_date
-			FROM `tabSerial and Batch Entry` sbe
-			JOIN `tabSerial and Batch Bundle` sbb ON sbe.parent = sbb.name AND sbb.docstatus = 1
-			WHERE sbb.voucher_type = 'Delivery Note'
-		) dn_bundle ON dn_bundle.serial_no = sn.name
-		LEFT JOIN (
-			SELECT 
-				sbe.serial_no, sbb.voucher_no AS voucher_no,sbb.posting_date
-			FROM `tabSerial and Batch Entry` sbe
-			JOIN `tabSerial and Batch Bundle` sbb ON sbe.parent = sbb.name AND sbb.docstatus = 1
-			WHERE sbb.voucher_type = 'Sales Invoice'
-		) si_bundle ON si_bundle.serial_no = sn.name
+			left outer join 
+			`tabInstallation Details CT` in_item ON in_item.ac_serial_no=sn.name and in_item.parent LIKE CONCAT('%', 'SINV-', '%')
+			left outer JOIN 
+			`tabSales Invoice` si on si.name=in_item.parent
+			left outer join 
+			`tabInstallation Details CT` dn_item ON dn_item.ac_serial_no=sn.name and dn_item.parent LIKE CONCAT('%', 'MAT-DN', '%')
+			left outer JOIN 
+			`tabDelivery Note` dn on dn.name=dn_item.parent
 		LEFT JOIN (
 			SELECT 
 				sbe.serial_no, sbb.voucher_no AS voucher_no,sbb.posting_date AS posting_date
 			FROM `tabSerial and Batch Entry` sbe
 			JOIN `tabSerial and Batch Bundle` sbb ON sbe.parent = sbb.name AND sbb.docstatus = 1
 			WHERE sbb.voucher_type = 'Stock Entry'
-		) se_bundle ON se_bundle.serial_no = sn.name  {0}""".format(conditions), as_dict=1, debug=1)
+		) se_bundle ON se_bundle.serial_no = sn.name 
+		where dn.docstatus != 2 AND si.docstatus != 2  {0}""".format(conditions), as_dict=1, debug=1)
 	return data
